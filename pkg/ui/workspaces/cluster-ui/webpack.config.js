44339ef94b22db333a96f1a887a2418b750a0cf4
Andrii Vorobiov <and.vorobiov@gmail.com>
2021-07-08 13:20:16 +0300

    ui: fix conflicts
    Release note: None

--- pkg/ui/workspaces/cluster-ui/webpack.config.js
+++ pkg/ui/workspaces/cluster-ui/webpack.config.js
@@ -144,8 +144,7 @@ module.exports = {
       root: "ReactDom",
     },
     // TODO (koorosh): commented out to make bazel build pass
-    // TODO (koorosh): commented out to make bazel build pass
-    //"react-router-dom": "react-router-dom",
+    "react-router-dom": "react-router-dom",
     "react-redux": "react-redux",
     "redux-saga": "redux-saga",
     "redux": "redux",
